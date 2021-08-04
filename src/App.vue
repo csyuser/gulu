94640d111e358078bd47f2aad66bf1ac953d3322
csyuser <13540039093@163.com>
2021-08-04 11:37:09 +0800

    手机屏幕时默认aside隐藏，且在每次切换路由之后隐藏aside

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,19 @@
 <script>
 
 import {ref,provide} from 'vue'
+import {router} from './router'
 
 export default {
   name: 'App',
   setup(){
-    const menuVisible = ref(false)
+    let width = document.documentElement.clientWidth
+    const menuVisible = ref(width > 500)
     provide('menuVisible',menuVisible)
+    router.afterEach(()=>{
+      if (width<=500){
+        menuVisible.value = false
+      }
+    })
   }
 }
 </script>
