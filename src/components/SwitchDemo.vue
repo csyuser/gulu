a130ea29b18fe83132b071d67a3225ab17c55125
csyuser <13540039093@163.com>
2021-12-06 15:41:11 +0800

    Switch 组件接受value和input事件

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,13 +1,18 @@
 <template>
-  <Switch></Switch>
+  <Switch :value="value" @input="value = $event"></Switch>
 </template>
 
 <script>
 import Switch from '../lib/Switch.vue'
+import {ref} from 'vue'
 
 export default {
   name: 'SwitchDemo',
   components: {Switch},
+  setup(){
+    const value = ref(false)
+    return {value}
+  }
 }
 </script>
 
--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,20 +1,21 @@
 <template>
-  <button :class="{checked}" @click="toggle">
+  <button :class="{checked:value}" @click="toggle">
     <span></span>
   </button>
 </template>
 
 <script lang="ts">
-import {ref} from 'vue'
 
 export default {
   name: 'Switch',
-  setup(){
-    const checked = ref(false)
+  props:{
+    value:{type:Boolean}
+  },
+  setup(props,context){
     const toggle = ()=>{
-      checked.value = !checked.value
+      context.emit('input',!props.value)
     }
-    return {checked,toggle}
+    return {toggle}
   }
 };
 </script>
