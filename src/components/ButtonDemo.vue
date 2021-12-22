e00aa716e0a0998fca171e5a9f894274738bb06f
csyuser <13540039093@163.com>
2021-12-22 11:31:19 +0800

    button组件支持click,mouseover,focus事件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,7 +1,7 @@
 <template>
   <section>
     <span>不同的theme：</span>
-    <Button>普通按钮</Button>
+    <Button @mouseover="mouseover" @click="click" @focus="focus">普通按钮</Button>
     <Button theme="button">普通按钮</Button>
     <Button theme="text">文字按钮</Button>
     <Button theme="link">链接</Button>
@@ -14,6 +14,18 @@ import Button from '../lib/Button.vue'
 export default {
   name: 'ButtonDemo',
   components: {Button},
+  setup(){
+    const mouseover = ()=>{
+      console.log('mouseover')
+    }
+    const click = ()=>{
+      console.log('click')
+    }
+    const focus = ()=>{
+      console.log('focus')
+    }
+    return {mouseover,click,focus}
+  }
 }
 </script>
 
