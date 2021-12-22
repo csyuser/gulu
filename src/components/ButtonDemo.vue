b2d643615d02fa7cf8c5b8ec7f3a9bdcb6a92017
csyuser <13540039093@163.com>
2021-12-22 11:10:48 +0800

    button组件基础样式

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,10 +1,18 @@
 <template>
-<div>ButtonDemo</div>
+  <section>
+    <span>不同的theme：</span>
+    <Button>普通按钮</Button>
+    <Button>文字按钮</Button>
+    <Button>链接</Button>
+  </section>
 </template>
 
 <script>
+import Button from '../lib/Button.vue'
+
 export default {
-name: "ButtonDemo"
+  name: 'ButtonDemo',
+  components: {Button},
 }
 </script>
 
