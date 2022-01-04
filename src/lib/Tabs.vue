45082d2d9bdeac6d79761afe5f8617995c9a7ae4
csyuser <13540039093@163.com>
2022-01-04 16:35:50 +0800

    tab 使用watchEffect

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="pipi-tabs">
     <div class="pipi-tabs-nav" ref="navContainer">
-      <div class="pipi-tabs-nav-item" v-for="item in navs" :key="item.name" :class="{'pipi-selected':item.name === activeName}"
-          :ref="setItemRef" @click="select(item.name)">{{ item.title }}</div>
+      <div class="pipi-tabs-nav-item" v-for="item in navs" :key="item.name"
+           :class="{'pipi-selected':item.name === activeName}"
+           :ref="setItemRef" @click="select(item.name)">{{ item.title }}
+      </div>
       <div class="pipi-tabs-nav-indicator" ref="indicator"></div>
     </div>
     <div class="pipi-tabs-content">
@@ -12,31 +14,30 @@
 </template>
 
 <script lang="ts">
-import {ref,onMounted,onUpdated,computed} from 'vue';
+import {ref, onMounted, onUpdated, computed, watchEffect} from 'vue';
 import TabPane from './TabPane.vue';
 
 export default {
   name: 'Tabs',
-  props:{
-    activeName:{type:String}
+  props: {
+    activeName: {type: String}
   },
   setup(props, context) {
-    const selectedNav = ref<HTMLDivElement>(null)
-    const indicator = ref<HTMLDivElement>(null)
-    const navContainer = ref<HTMLDivElement>(null)
+    const selectedNav = ref<HTMLDivElement>(null);
+    const indicator = ref<HTMLDivElement>(null);
+    const navContainer = ref<HTMLDivElement>(null);
 
 //设置选中标题nav的宽以及位置
-    const x = ()=>{
-      let {width} = selectedNav.value.getBoundingClientRect()
-      let {left:left1} = navContainer.value.getBoundingClientRect()
-      let {left:left2} = selectedNav.value.getBoundingClientRect()
-      let xxx = left2 - left1
-      indicator.value.style.width = `${width}px`
-      indicator.value.style.left = `${xxx}px`
-    }
-
-    onMounted(x)
-    onUpdated(x)
+    onMounted(() => {
+      watchEffect(() => {
+        let {width} = selectedNav.value.getBoundingClientRect();
+        let {left: left1} = navContainer.value.getBoundingClientRect();
+        let {left: left2} = selectedNav.value.getBoundingClientRect();
+        let left = left2 - left1;
+        indicator.value.style.width = `${width}px`;
+        indicator.value.style.left = `${left}px`;
+      });
+    });
 //获取tabs组件的默认slot，并判断只能是TabPane标签
     const defaultSlots = context.slots.default();
     defaultSlots.forEach((item) => {
@@ -49,21 +50,21 @@ export default {
       return item.props;
     });
 //设置navItem的ref，找到选中的navItem
-    const setItemRef = (el)=>{
-        if(el?.classList.contains('pipi-selected')){
-          selectedNav.value = el;
-        }
-    }
+    const setItemRef = (el) => {
+      if (el?.classList.contains('pipi-selected')) {
+        selectedNav.value = el;
+      }
+    };
 //标题nav的select事件
-    const select = (name:String)=>{
-      context.emit('update:activeName',name)
-    }
-    const current = computed(()=>{
-      return defaultSlots.filter((item)=>{
-       return  item.props.name === props.activeName
-      })[0]
-    })
-    return {defaultSlots, navs,setItemRef,indicator,navContainer,select,current};
+    const select = (name: String) => {
+      context.emit('update:activeName', name);
+    };
+    const current = computed(() => {
+      return defaultSlots.filter((item) => {
+        return item.props.name === props.activeName;
+      })[0];
+    });
+    return {defaultSlots, navs, setItemRef, indicator, navContainer, select, current};
   }
 };
 </script>
@@ -83,11 +84,20 @@ export default {
       padding: 8px 0;
       cursor: pointer;
 
-      &:first-child {margin-left: 0}
-      &:last-child {margin-right: 0}
-      &.pipi-selected{color: $blue;}
+      &:first-child {
+        margin-left: 0
+      }
+
+      &:last-child {
+        margin-right: 0
+      }
+
+      &.pipi-selected {
+        color: $blue;
+      }
     }
-    &-indicator{
+
+    &-indicator {
       position: absolute;
       //left: 0;
       bottom: -2px;
@@ -96,7 +106,8 @@ export default {
       transition: all 250ms;
     }
   }
-  &-content{
+
+  &-content {
     padding: 8px 0;
     color: $color;
   }
