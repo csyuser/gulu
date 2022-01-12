d535ff6e63b59674905b16eec8522261aee037f4
csyuser <13540039093@163.com>
2022-01-12 16:21:33 +0800

    引入markDown样式

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from './App.vue';
 import './lib/pipi.scss'
 import './index.scss';
 import {router} from './router';
+import 'github-markDown-css'
 
 const app = createApp(App);
 app.use(router);
