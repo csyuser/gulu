fa906ccd9d2185ff28a50120609c8db7930653b1
csyuser <13540039093@163.com>
2022-01-14 17:18:02 +0800

    封装docDemo

---
+++ src/components/DocDemo.vue
@@ -0,0 +1,65 @@
+<template>
+  <section>
+    <!--    <div class="example">-->
+    <header>
+      <h3>{{ title }}</h3>
+      <div class="tools">
+        <svg class="icon">
+          <use xlink:href="#icon-code"></use>
+        </svg>
+      </div>
+    </header>
+    <main>
+      <div class="description">
+        <slot name="description"></slot>
+      </div>
+      <div class="example">
+        <slot name="example"></slot>
+      </div>
+      <div class="code">
+        <slot name="code"></slot>
+      </div>
+    </main>
+    <!--    </div>-->
+  </section>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'DocDemo',
+  props: {title: {type:String}}
+}
+</script>
+
+<style scoped lang='scss'>
+$borderColor: #dcdfe6;
+
+section {
+  header {
+    display: flex;
+    align-items: center;
+    border-bottom: 1px solid $borderColor;
+    padding: 16px;
+
+    > .tools {
+      margin-left: auto;
+
+      .icon {
+        cursor: pointer;
+
+        &:hover {
+          color: lighten(#24292E, 20%)
+        }
+      }
+    }
+  }
+
+  > main {
+    padding: 16px;
+  }
+
+  margin-top: 16px;
+  border: 1px solid $borderColor;
+  border-radius: 4px;
+}
+</style>
--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,38 +1,37 @@
 <template>
-  <section>
-    <span>正常状态：</span>
-    <Switch v-model:value="value"></Switch>
-  </section>
-  <section>
-    <span>禁用状态：</span>
-    <Switch v-model:value="value2" disabled></Switch>
-  </section>
-  <section>
-    <span>自定义颜色：</span>
-    <Switch v-model:value="value3" active-color="green" inactive-color="red"></Switch>
-  </section>
+  <DocDemo title="正常状态">
+    <template #example>
+      <Switch v-model:value="value"></Switch>
+    </template>
+  </DocDemo>
+  <DocDemo title="禁用状态">
+    <template #example>
+      <Switch v-model:value="value2" disabled></Switch>
+    </template>
+  </DocDemo>
+  <DocDemo title="自定义颜色">
+    <template #example>
+      <Switch v-model:value="value3" active-color="green" inactive-color="red"></Switch>
+    </template>
+  </DocDemo>
 </template>
 
 <script>
 import Switch from '../lib/Switch.vue'
 import {ref} from 'vue'
+import DocDemo from './DocDemo.vue'
 
 export default {
   name: 'SwitchDemo',
-  components: {Switch},
+  components: {Switch, DocDemo},
   setup() {
     const value = ref(true)
     const value2 = ref(false)
     const value3 = ref(false)
-    return {value, value2,value3}
+    return {value, value2, value3}
   }
 }
 </script>
 
 <style scoped lang='scss'>
-section {
-  display: flex;
-  align-items: center;
-  margin-top: 16px;
-}
 </style>
