31ac2343d3fcfcf24d9bae70abfc157627986fc0
csyuser <13540039093@163.com>
2022-01-18 15:07:09 +0800

    重新封装docDemo

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,18 +1,11 @@
 <template>
-  <DocDemo title="常规用法">
-    <template #example>
-      <SwitchNormal></SwitchNormal>
-    </template>
-    <template #code>
-      <pre class="language-css" v-html="Prism.highlight(SwitchNormal['__sourceCode'], Prism.languages.html, 'html')"/>
-    </template>
-  </DocDemo>
-  <DocDemo title="禁用状态">
+  <DocDemo :component="SwitchNormal"></DocDemo>
+  <DocDemo>
     <template #example>
       <Switch v-model:value="value2" disabled></Switch>
     </template>
   </DocDemo>
-  <DocDemo title="自定义颜色">
+  <DocDemo>
     <template #example>
       <Switch v-model:value="value3" active-color="green" inactive-color="red"></Switch>
     </template>
@@ -25,18 +18,15 @@ import {ref} from 'vue'
 import DocDemo from './DocDemo.vue'
 import Markdown from './Markdown.vue'
 import SwitchNormal from './switch-demo/SwitchNormal.vue'
-import 'prismjs'
-import 'prismjs/themes/prism-okaidia.css'
-const Prism = (window as any).Prism
 
 
 export default {
   name: 'SwitchDemo',
-  components: {Switch, DocDemo,Markdown,SwitchNormal},
+  components: {Switch, DocDemo,Markdown},
   setup() {
     const value2 = ref(false)
     const value3 = ref(false)
-    return {value2, value3,SwitchNormal,Prism}
+    return {value2, value3,SwitchNormal}
   }
 }
 </script>
