bef40e6741910eefd10a4304dadb110b398769af
csyuser <13540039093@163.com>
2022-01-18 13:49:43 +0800

    使用prism高亮代码

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,18 +1,10 @@
 <template>
-<!--  <DocDemo title="常规用法">-->
-<!--    <template #example>-->
-<!--      <Switch v-model:value="value"></Switch>-->
-<!--    </template>-->
-<!--    <template #code>-->
-<!--      <Markdown path="../markdown/switch.md"></Markdown>-->
-<!--    </template>-->
-<!--  </DocDemo>-->
   <DocDemo title="常规用法">
     <template #example>
       <SwitchNormal></SwitchNormal>
     </template>
     <template #code>
-      <pre>{{SwitchNormal['__sourceCode']}}</pre>
+      <pre class="language-css" v-html="Prism.highlight(SwitchNormal['__sourceCode'], Prism.languages.html, 'html')"/>
     </template>
   </DocDemo>
   <DocDemo title="禁用状态">
@@ -27,12 +19,16 @@
   </DocDemo>
 </template>
 
-<script>
+<script lang="ts">
 import Switch from '../lib/Switch.vue'
 import {ref} from 'vue'
 import DocDemo from './DocDemo.vue'
 import Markdown from './Markdown.vue'
 import SwitchNormal from './switch-demo/SwitchNormal.vue'
+import 'prismjs'
+import 'prismjs/themes/prism-okaidia.css'
+const Prism = (window as any).Prism
+
 
 export default {
   name: 'SwitchDemo',
@@ -40,7 +36,7 @@ export default {
   setup() {
     const value2 = ref(false)
     const value3 = ref(false)
-    return {value2, value3,SwitchNormal}
+    return {value2, value3,SwitchNormal,Prism}
   }
 }
 </script>
