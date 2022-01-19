9de931ba9b98c2c3a1062934ad11bfab9a26b3ab
csyuser <13540039093@163.com>
2022-01-19 16:06:29 +0800

    tab组件文档ok

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,37 +1,16 @@
 <template>
-  <section>
-    <p>基本用法：</p>
-    <Tabs v-model:active-name="activeName">
-      <TabPane title="标题1" name="first">内容1</TabPane>
-      <TabPane title="标题2222" name="second">内容2222</TabPane>
-      <TabPane title="标题333333333" name="third">内容333333333</TabPane>
-    </Tabs>
-  </section>
+  <DocDemo :component="TabNormal"/>
 </template>
 
 <script>
-import {ref} from 'vue'
-import Tabs from '../lib/Tabs.vue'
-import TabPane from '../lib/TabPane.vue'
+import DocDemo from './DocDemo.vue'
+import TabNormal from './tab-demo/TabNormal.vue'
 
 export default {
   name: 'TabsDemo',
-  components: {Tabs, TabPane},
+  components:{DocDemo},
   setup(){
-    const activeName = ref('second')
-    return {activeName}
+    return {TabNormal}
   },
 }
 </script>
-
-<style scoped lang='scss'>
-section {
-  //display: flex;
-  //align-items: center;
-  margin-top: 16px;
-
-  p {
-    line-height: 1.5;
-  }
-}
-</style>
---
+++ src/components/tab-demo/TabNormal.vue
@@ -0,0 +1,24 @@
+<demo>常规用法</demo>
+
+<template>
+  <Tabs v-model:active-name="activeName">
+    <TabPane title="标题1" name="first">内容1</TabPane>
+    <TabPane title="标题2222" name="second">内容2222</TabPane>
+    <TabPane title="标题333333333" name="third">内容333333333</TabPane>
+  </Tabs>
+</template>
+
+<script lang="ts">
+import {ref} from 'vue'
+import Tabs from '../../lib/Tabs.vue';
+import TabPane from '../../lib/TabPane.vue';
+
+export default {
+  name: 'SwitchNormal',
+  components: {Tabs, TabPane},
+  setup(){
+    const activeName = ref('second')
+    return {activeName}
+  },
+}
+</script>
